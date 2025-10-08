f50b4473153ca21e3a65e646bdf2bf5682fd5ee9
Franchuteelmastop <francoramospaypal@gmail.com>
2025-10-07 23:10:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import ImageUploader from './components/ImageUploader'
+import Contact from './components/Contact'
+import Services from './components/Services'
+
+function Home() {
+  return (
+    <main className="container">
+      <h1>Visualizador de Imágenes</h1>
+      <ImageUploader />
+    </main>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servicios" element={<Services />} />
+        <Route path="/contacto" element={<Contact />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
