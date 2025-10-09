935b48e9232e39d344578401de6d4ecfc0e7fb76
Franchuteelmastop <francoramospaypal@gmail.com>
2025-10-09 17:04:31 -0300

    Update rooms.js

--- src/data/rooms.js
+++ src/data/rooms.js
@@ -5,23 +5,23 @@ const rooms = [
     description: 'Habitación cómoda con cama individual, baño privado y Wi-Fi.',
     price: '$3,000 ARS / noche',
     capacity: 1,
-    image: 'https://images.unsplash.com/photo-1611892440504-42a792e24d32?auto=format&fit=crop&w=800&q=60'
+    image: 'https://images.unsplash.com/photo-1582719478250-c89cae4dc85b?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 2,
     name: 'Habitación Doble',
-    description: 'Habitación con dos camas individuales o una matrimonial, TV y escritorio.',
+    description: 'Habitación amplia con dos camas individuales, TV y escritorio.',
     price: '$5,000 ARS / noche',
     capacity: 2,
-    image: 'https://images.unsplash.com/photo-1501117716987-c8e1ecb210c7?auto=format&fit=crop&w=800&q=60'
+    image: 'https://images.unsplash.com/photo-1505691723518-36a5ac3be353?auto=format&fit=crop&w=800&q=80'
   },
   {
     id: 3,
     name: 'Suite Ejecutiva',
-    description: 'Suite espaciosa con cama matrimonial, sala de estar y vista panorámica.',
+    description: 'Suite elegante con cama matrimonial, sala de estar y vista panorámica.',
     price: '$9,000 ARS / noche',
     capacity: 2,
-    image: 'https://images.unsplash.com/photo-1505691938895-1758d7feb511?auto=format&fit=crop&w=800&q=60'
+    image: 'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=800&q=80'
   }
 ]
 
